48a6a7998580d2f27c67996642bb63d38870dc8f
Ryan Gordon <rgordon@gunnjerkens.com>
2015-07-16 17:26:19 -0700

    Closes #5 -- Added credit as comment in template head, removed rogue html tag

--- template.php
+++ template.php
@@ -8,8 +8,8 @@
 
 <!DOCTYPE html>
 <html class="no-js">
-<html>
   <head>
+    <!-- This site is currently in maintenance mode brought to you by WP Chill Mode. Made with love by Ryan Gordon | http://supryan.com -->
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
